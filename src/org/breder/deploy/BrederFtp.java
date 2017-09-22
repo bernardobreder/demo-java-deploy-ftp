2e18bd449935f2a5b583da64ee546a518b1e1c25
Bernardo Breder <bernardobreder@gmail.com>
2017-09-22 20:59:22 -0300

    .

---
+++ src/org/breder/deploy/BrederFtp.java
@@ -0,0 +1,36 @@
+package org.breder.deploy;
+
+import java.io.IOException;
+
+/**
+ * Ftp do breder.org
+ * 
+ * 
+ * @author Bernardo Breder
+ */
+public class BrederFtp extends Ftp {
+
+	/**
+	 * Construtor
+	 * 
+	 * @param password
+	 * @param path
+	 *            webapps/pub/abc.jpg
+	 * @throws IOException
+	 */
+	public BrederFtp(String password, String path) throws IOException {
+		super("breder.org", "breder", password, "../../" + path);
+	}
+
+	/**
+	 * Construtor
+	 * 
+	 * @param path
+	 *            webapps/pub/abc.jpg
+	 * @throws IOException
+	 */
+	public BrederFtp(String path) throws IOException {
+		this(new String(BOptionPane.showPasswordDialog(null, "Passowrd", "Password of breder.org")), path);
+	}
+
+}
